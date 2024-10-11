4dc1f33b7088402eda31c72e09b4941a9b4e08c3
Rombt <rombtnet@gmail.com>
2024-10-11 14:57:20 +0300

    .

--- app/src/assets/js/modules/spoiler.js
+++ app/src/assets/js/modules/spoiler.js
@@ -1,47 +1,128 @@
 /*
  *
- * 	Simple spoiler
  *	The styles in the file spoiler.less
  *
  */
 
-function spoiler() {
-  const spoilers = document.querySelectorAll('.spoilers__title');
+class Spoiler {
+  heightSpoilersBlock;
+  marginBottomSpoiler;
+  heightParentBlock;
 
-  spoilers.forEach(spoiler => {
-    spoiler.addEventListener('click', e => {
-      const spoilerBody = e.target
-        .closest('.spoilers__item')
-        .querySelector('.spoilers__body');
+  flagSpoilerOpen = false;
 
-      spoilerBody.classList.toggle('spoilers__body_open');
-      e.target.classList.toggle('spoilers__title_open');
+  constructor(spoilersBlock, param) {
+    this.spoilersBlock = spoilersBlock;
 
-      if (typeof gsap !== 'undefined') {
-        if (spoilerBody.classList.contains('open')) {
-          if (spoilerBody) {
-            gsap.to(spoilerBody, {
-              duration: 0.5,
-              ease: 'power1.in',
-              height: 'auto',
-              overflow: 'visible',
-              pointerEvents: 'auto',
-              opacity: 1,
-            });
-          }
-        } else {
-          gsap.to(spoilerBody, {
-            duration: 0.5,
-            ease: 'power1.in',
-            height: '0px',
-            overflow: 'hidden',
-            pointerEvents: 'none',
-            opacity: 0,
-          });
-        }
+    this.classSpoiler = param.classSpoiler;
+    this.classSpoilerTitle = param.classSpoilerTitle;
+    this.classSpoilerBody = param.classSpoilerBody;
+    this.classSpoilerOpen = param.classSpoilerOpen;
+    this.classSpoilerTitleOpen = param.classSpoilerTitleOpen;
+    this.classSpoilerBodyOpen = param.classSpoilerBodyOpen;
+    this.parentBlock = this.spoilersBlock.parentElement;
+
+    this.spoilers = this.spoilersBlock.querySelectorAll(`.${this.classSpoiler}`);
+    this.setPositionSpoilers();
+    this.clickProcessing();
+
+    const observer = new ResizeObserver(entries => {
+      if (!this.heightParentBlock) {
+        this.getHeightSpoilersBlockParent();
+      }
+
+      if (!this.flagSpoilerOpen) {
+        this.setPositionSpoilers();
       }
     });
-  });
+    observer.observe(this.parentBlock);
+  }
+
+  clickProcessing() {
+    this.spoilers.forEach(spoiler => {
+      spoiler.addEventListener('click', e => {
+        const spoiler = e.target.closest(`.${this.classSpoiler}`);
+        const spoilerTitle = spoiler.querySelector(`.${this.classSpoilerTitle}`);
+        const spoilerBody = spoiler.querySelector(`.${this.classSpoilerBody}`);
+
+        spoiler.classList.toggle(this.classSpoilerOpen);
+        spoilerTitle.classList.toggle(this.classSpoilerTitleOpen);
+        spoilerBody.classList.toggle(this.classSpoilerBodyOpen);
+
+        if (spoilerBody) {
+          if (spoilerBody.classList.contains(this.classSpoilerBodyOpen)) {
+            spoilerBody.style.height = 'auto';
+            const autoHeightSpoilerBody = spoilerBody.scrollHeight;
+            spoilerBody.style.height = '0px';
+            spoilerBody.getBoundingClientRect();
+            this.spoilersBlock.style.maxHeight =
+              this.heightParentBlock + autoHeightSpoilerBody + 'px'; //!!
+            spoilerBody.style.height = autoHeightSpoilerBody + 'px';
+
+            this.flagSpoilerOpen = true;
+          } else {
+            spoilerBody.getBoundingClientRect();
+            spoilerBody.style.height = '0px';
+            this.spoilersBlock.style.maxHeight = this.heightParentBlock + 'px';
+            this.flagSpoilerOpen = false;
+          }
+        }
+      });
+    });
+  }
+
+  setPositionSpoilers() {
+    this.getHeightSpoilersBlock();
+    this.getSpoilerMarginBottom();
+    [...this.spoilers].map((spoiler, index) => {
+      spoiler.style.marginBottom = this.marginBottomSpoiler + 'px';
+    });
+  }
+
+  getHeightSpoilersBlockParent() {
+    this.heightParentBlock = this.parentBlock.getBoundingClientRect().height;
+    this.spoilersBlock.style.maxHeight = this.heightParentBlock + 'px';
+  }
+
+  getHeightSpoilersBlock() {
+    const style = window.getComputedStyle(this.spoilersBlock);
+    const paddingTop = parseFloat(style.paddingTop);
+    const paddingBottom = parseFloat(style.paddingBottom);
+    this.heightSpoilersBlock =
+      this.spoilersBlock.clientHeight - paddingTop - paddingBottom;
+  }
+
+  getSpoilerMarginBottom() {
+    const sumHeightSpoilers = [...this.spoilers].reduce((sumHeight, spoiler) => {
+      const heightSpoiler = spoiler.clientHeight;
+      const spoilerTitle = spoiler.querySelector(`.${this.classSpoilerTitle}`);
+      const spoilerBody = spoiler.querySelector(`.${this.classSpoilerBody}`);
+      const heightSpoilerBody = spoilerBody.offsetHeight;
+      const heightSpoilerWithBorder = spoiler.offsetHeight - heightSpoilerBody;
+
+      spoilerTitle.style.height = heightSpoiler - heightSpoilerBody + 'px';
+      spoilerTitle.style.lineHeight = heightSpoiler - heightSpoilerBody + 'px';
+      return sumHeight + heightSpoilerWithBorder;
+    }, 0);
+
+    this.marginBottomSpoiler =
+      (this.heightSpoilersBlock - sumHeightSpoilers) / (this.spoilers.length - 1);
+  }
 }
 
-spoiler();
+document.addEventListener('DOMContentLoaded', function () {
+  const param = {
+    classSpoilersBlock: 'spoilers-block',
+    classSpoiler: 'spoiler',
+    classSpoilerTitle: 'spoiler__title',
+    classSpoilerBody: 'spoiler__body',
+
+    classSpoilerOpen: '_open-spoiler',
+    classSpoilerTitleOpen: '_open-title',
+    classSpoilerBodyOpen: '_open-body',
+  };
+
+  document.querySelectorAll(`.${param.classSpoilersBlock}`).forEach(spoilersBlock => {
+    new Spoiler(spoilersBlock, param);
+  });
+});
